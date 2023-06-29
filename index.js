f27eed7f3abaceca1da4727a5227017904bae0d2
alexb017 <24658786+alexb017@users.noreply.github.com>
2023-06-29 03:29:58 +0300

    commit

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
-const tabs = document.querySelectorAll('.tab');
-tabs.forEach((tab) => {
-    tab.addEventListener('click', (e) => {
-        document.querySelector('.tab.active').classList.remove('active');
-
-        e.currentTarget.classList.add('active');
-
-        document.querySelector('.tab-content.show').classList.remove('show');
-
-        const selector = e.currentTarget.dataset.content;
-        document.querySelector(selector).classList.add('show');
-    });
-});
-
-const btnAppearance = document.querySelector('.btn-appearance');
-const lightIcon = document.querySelector('.light-icon');
+const btnDarkMode = document.querySelector('.btn-dark-mode');
 const darkIcon = document.querySelector('.dark-icon');
+const lightIcon = document.querySelector('.light-icon');
 
 const theme = localStorage.getItem('theme');
 
 if (theme) {
     document.body.classList.add(theme);
-    lightIcon.classList.remove('show');
-    darkIcon.classList.add('show');
+    darkIcon.classList.remove('show');
+    lightIcon.classList.add('show');
 }
 
-btnAppearance.addEventListener('click', () => {
+btnDarkMode.addEventListener('click', () => {
     if (document.body.classList.contains('dark')) {
         document.body.classList.remove('dark');
-        darkIcon.classList.remove('show');
-        lightIcon.classList.add('show');
+        darkIcon.classList.add('show');
+        lightIcon.classList.remove('show');
         localStorage.setItem('theme', '')
     } else {
         document.body.classList.add('dark');
-        lightIcon.classList.remove('show');
-        darkIcon.classList.add('show');
+        darkIcon.classList.remove('show');
+        lightIcon.classList.add('show');
         localStorage.setItem('theme', 'dark');
     }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const itemsAbout = document.querySelectorAll('.slidein');
+
+    itemsAbout.forEach((item, index) => {
+        setTimeout(() => {
+            item.classList.add('fadein');
+        }, index * 100);
+    });
 });
