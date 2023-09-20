9e84a320c1791181a8b24d7d9e98e5a577cb2caa
alexb017 <alexbacirea@gmail.com>
2023-09-20 20:08:57 +0300

    commit

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const btnDark = document.querySelector('.btn-dark-mode');
 const theme = localStorage.getItem('theme');
 if (theme) {
   document.body.classList.add(theme);
+  btnDark.innerHTML = 'Use light theme';
 }
 
 btnDark.addEventListener('click', () => {
   if (document.body.classList.contains('dark')) {
     document.body.classList.remove('dark');
+    btnDark.innerHTML = 'Use dark theme';
     localStorage.setItem('theme', '');
   } else {
     document.body.classList.add('dark');
+    btnDark.innerHTML = 'Use light theme';
     localStorage.setItem('theme', 'dark');
   }
 });
