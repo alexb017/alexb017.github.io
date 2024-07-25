8de7035280303aa3d328ff66a56d6bc1b62bbf61
Alex <alexbacirea@gmail.com>
2024-07-25 14:37:29 +0300

    Adds btns to change the content for each project

---
+++ index.js
@@ -0,0 +1,14 @@
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', (event) => {
+    document.querySelector('.tab.active').classList.remove('active');
+
+    event.currentTarget.classList.add('active');
+
+    document.querySelector('.tab-content.show').classList.remove('show');
+
+    const selector = event.currentTarget.dataset.content;
+    document.querySelector(selector).classList.add('show');
+  });
+});
