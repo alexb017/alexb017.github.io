484cb9458190b1db9370635edbd942edb737700f
Alex <alexbacirea@gmail.com>
2024-10-01 23:50:17 +0300

    style: add some parts of new design

--- index.js
+++ index.js
@@ -32,85 +32,19 @@
 //   }, index * 200);
 // });
 
-// const btnsOpen = document.querySelectorAll('.card');
-// const btnsClose = document.querySelectorAll('.btn-close');
+const tabs = document.querySelectorAll('.tab');
 
-// btnsOpen.forEach((btn) => {
-//   btn.addEventListener('click', (event) => {
-//     if (event.currentTarget.classList.contains('card')) {
-//       const selector = event.currentTarget.dataset.dialog;
-//       const dialog = document.getElementById(selector);
-//       document.body.style.overflow = 'hidden';
-//       dialog.showModal();
-//     }
-//   });
-// });
+tabs.forEach((tab) => {
+  tab.addEventListener('click', (event) => {
+    document.querySelector('.tab.active').classList.remove('active');
 
-// btnsClose.forEach((btn) => {
-//   btn.addEventListener('click', (event) => {
-//     if (event.currentTarget.classList.contains('btn-close')) {
-//       const selector = event.currentTarget.dataset.close;
-//       const dialog = document.getElementById(selector);
-//       dialog.setAttribute('closing', '');
-//       dialog.addEventListener(
-//         'animationend',
-//         () => {
-//           dialog.removeAttribute('closing');
-//           document.body.style.overflow = 'auto';
-//           dialog.close();
-//         },
-//         { once: true }
-//       );
-//     }
-//   });
-// });
+    event.currentTarget.classList.add('active');
 
-// Carousel
-const sliderCarousel = document.querySelector('.carousel');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
+    document
+      .querySelector('.tab-content.tab-show')
+      .classList.remove('tab-show');
 
-function updateButtons() {
-  // Disable the prev button when the carousel is at the beginning
-  if (sliderCarousel.scrollLeft === 0) {
-    prev.disabled = true;
-  } else {
-    prev.disabled = false;
-  }
-
-  // Disable next button if the carousel is at the end
-  if (
-    sliderCarousel.scrollLeft + sliderCarousel.clientWidth >=
-    sliderCarousel.scrollWidth - 1
-  ) {
-    next.disabled = true;
-  } else {
-    next.disabled = false;
-  }
-}
-
-// Move the carousel to the right
-function moveRight() {
-  sliderCarousel.scrollBy({
-    left: sliderCarousel.clientWidth / 4,
-    behavior: 'smooth',
+    const selector = event.currentTarget.dataset.tab;
+    document.querySelector(selector).classList.add('tab-show');
   });
-}
-
-// Move the carousel to the left
-function moveLeft() {
-  sliderCarousel.scrollBy({
-    left: -sliderCarousel.clientWidth / 4,
-    behavior: 'smooth',
-  });
-}
-
-// Initial button state
-updateButtons();
-
-// Add event listeners to the buttons
-next.addEventListener('click', moveRight);
-prev.addEventListener('click', moveLeft);
-
-// Add event listener to update the buttons when the carousel scrolls
-sliderCarousel.addEventListener('scroll', updateButtons);
+});
