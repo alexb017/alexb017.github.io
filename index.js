75d46a469a947965c956086c2ceb6727b56af2d8
Alex <alexbacirea@gmail.com>
2024-10-01 17:31:10 +0300

    style: add new style

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const emailAddress = 'alexbacirea@gmail.com';
+// const emailAddress = 'alexbacirea@gmail.com';
 
-const copyEmailBtn = document.querySelector('.btn-email');
-const popup = document.querySelector('.email-popup');
+// const copyEmailBtn = document.querySelector('.btn-email');
+// const popup = document.querySelector('.email-popup');
 
-copyEmailBtn.addEventListener('click', (event) => {
-  navigator.clipboard.writeText(emailAddress);
+// copyEmailBtn.addEventListener('click', (event) => {
+//   navigator.clipboard.writeText(emailAddress);
 
-  // Remove fade-out class before showing the popup
-  popup.classList.remove('fade-out');
-  popup.classList.remove('hidden');
+//   // Remove fade-out class before showing the popup
+//   popup.classList.remove('fade-out');
+//   popup.classList.remove('hidden');
 
-  setTimeout(() => {
-    popup.classList.add('fade-out');
+//   setTimeout(() => {
+//     popup.classList.add('fade-out');
 
-    // Wait for the animation to finish before hiding the popup
-    popup.addEventListener(
-      'animationend',
-      () => {
-        popup.classList.add('hidden');
-      },
-      { once: true }
-    );
-  }, 2000);
-});
+//     // Wait for the animation to finish before hiding the popup
+//     popup.addEventListener(
+//       'animationend',
+//       () => {
+//         popup.classList.add('hidden');
+//       },
+//       { once: true }
+//     );
+//   }, 2000);
+// });
 
-const slideElements = document.querySelectorAll('.slide-enter');
+// const slideElements = document.querySelectorAll('.slide-enter');
 
-slideElements.forEach((element, index) => {
-  setTimeout(() => {
-    element.classList.add('slide-enter-active');
-  }, index * 200);
-});
+// slideElements.forEach((element, index) => {
+//   setTimeout(() => {
+//     element.classList.add('slide-enter-active');
+//   }, index * 200);
+// });
 
 // const btnsOpen = document.querySelectorAll('.card');
 // const btnsClose = document.querySelectorAll('.btn-close');
@@ -64,3 +64,53 @@ slideElements.forEach((element, index) => {
 //     }
 //   });
 // });
+
+// Carousel
+const sliderCarousel = document.querySelector('.carousel');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+function updateButtons() {
+  // Disable the prev button when the carousel is at the beginning
+  if (sliderCarousel.scrollLeft === 0) {
+    prev.disabled = true;
+  } else {
+    prev.disabled = false;
+  }
+
+  // Disable next button if the carousel is at the end
+  if (
+    sliderCarousel.scrollLeft + sliderCarousel.clientWidth >=
+    sliderCarousel.scrollWidth - 1
+  ) {
+    next.disabled = true;
+  } else {
+    next.disabled = false;
+  }
+}
+
+// Move the carousel to the right
+function moveRight() {
+  sliderCarousel.scrollBy({
+    left: sliderCarousel.clientWidth / 4,
+    behavior: 'smooth',
+  });
+}
+
+// Move the carousel to the left
+function moveLeft() {
+  sliderCarousel.scrollBy({
+    left: -sliderCarousel.clientWidth / 4,
+    behavior: 'smooth',
+  });
+}
+
+// Initial button state
+updateButtons();
+
+// Add event listeners to the buttons
+next.addEventListener('click', moveRight);
+prev.addEventListener('click', moveLeft);
+
+// Add event listener to update the buttons when the carousel scrolls
+sliderCarousel.addEventListener('scroll', updateButtons);
