87a16af04ddb416d2a75af39de8dab3691aec244
Alex <alexbacirea@gmail.com>
2024-10-12 13:38:09 +0300

    style: add some new changes

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-const emailAddress = 'alexbacirea@gmail.com';
-
-const copyEmailBtn = document.querySelector('.btn-email');
-
-copyEmailBtn.addEventListener('click', (event) => {
-  const originalText = copyEmailBtn.textContent.trim();
-  console.log(originalText);
-  copyEmailBtn.childNodes[copyEmailBtn.childNodes.length - 1].textContent =
-    'Copied!';
-
-  setTimeout(() => {
-    navigator.clipboard.writeText(emailAddress).then(() => {
-      copyEmailBtn.childNodes[
-        copyEmailBtn.childNodes.length - 1
-      ].textContent = ` ${originalText}`;
-    });
-  }, 1000);
-});
-
 const slideElements = document.querySelectorAll('.slide-enter');
 
 slideElements.forEach((element, index) => {
   setTimeout(() => {
     element.classList.add('slide-enter-active');
-  }, index * 100);
+  }, index * 150);
 });
