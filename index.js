53dde83bd2f445d967d2f71938b3a128ee370a4a
Alex <alexbacirea@gmail.com>
2024-12-18 19:54:21 +0200

    feat: add slide in animation for each element on the page

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const slideInElements = document.querySelectorAll('.slide-in');
+const slideNav = document.querySelector('.slide-nav');
 
+slideInElements.forEach((slideInElement, index) => {
+  setTimeout(() => {
+    slideInElement.classList.add('active');
+  }, index * 150);
+
+  slideNav.classList.add('active');
+});
