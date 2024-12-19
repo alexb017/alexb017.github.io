05ad663618e620e6d3341a5882994c836ae21365
Alex <alexbacirea@gmail.com>
2024-12-19 03:12:20 +0200

    style(ui/ux): made a few changes to navbar

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const slideInElements = document.querySelectorAll('.slide-in');
-const slideNav = document.querySelector('.slide-nav');
 
 slideInElements.forEach((slideInElement, index) => {
   setTimeout(() => {
     slideInElement.classList.add('active');
   }, index * 50);
-
-  slideNav.classList.add('active');
 });
