5e2446283613bd27b740a0f62031296af16d8116
alexb017 <alexbacirea@gmail.com>
2026-02-21 23:52:46 +0200

    ...

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Image follow cursor on hover
 const itemLinks = document.querySelectorAll(".item-link");
 itemLinks.forEach((link) => {
-  const project = link.closest(".project");
+  // const project = link.closest(".project");
 
-  link.addEventListener("mouseenter", () => {
-    project.classList.add("active");
-  });
+  // link.addEventListener("mouseenter", () => {
+  //   project.classList.add("active");
+  // });
 
-  link.addEventListener("mouseleave", () => {
-    project.classList.remove("active");
-  });
+  // link.addEventListener("mouseleave", () => {
+  //   project.classList.remove("active");
+  // });
 
   const img = link.querySelector("img");
   link.addEventListener("mousemove", (e) => {
@@ -43,7 +43,7 @@ function updateTime() {
   const delay = 1000 - now.getMilliseconds();
   setTimeout(updateTime, delay);
 }
-updateTime();
+// updateTime();
 
 // Fade in animation on load
 const fadeInElements = document.querySelectorAll(".fade-in");
